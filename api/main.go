0521c017c57cba85f0166696b08beaff2760ae8a
v.seleckij <v.seleckij@gmail.com>
2025-09-04 17:27:58 +0300

    upload new file

--- api/main.go
+++ api/main.go
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:df1d036cbbf3df46e2045071e082245ece204c7f53ecf0a4e022bff9bb228f47
-size 13
+package main
+
+import "fmt"
+
+func main() { fmt.Println("Hello, World!") }
